afac6a696c914b9f13fc504255c46046c4520cd2
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-04-27 17:37:16 +0700

    chore: add smartcontract & abi

--- src/services/contractAddress.ts
+++ src/services/contractAddress.ts
@@ -1,3 +1,4 @@
-const contractAddress = "0xYourContractAddressHere";
+const contractAddressTestUSDC = "0x4e1D8e819Fd303D0bc50E6d52DEd95cF17587Ec5";
+const contractAddressYieldUSDC = "0x2ea1FAEFc79D7dA7b640bC6a29d2cb9f074b9172";
 
-export { contractAddress };
+export { contractAddressTestUSDC, contractAddressYieldUSDC };
